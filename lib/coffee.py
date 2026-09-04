b7ee9db1dfa207129689c9f3514a3e7d8f0e5941
Josephhh-web <pchaos24@gmail.com>
2026-09-04 09:35:28 +0300

    add completed Book and Coffee class implementations

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
-
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        # set initial values
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, new_size):
+
+        if new_size == "Small" or new_size == "Medium" or new_size == "Large":
+            self._size = new_size
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+
+        print("This coffee is splendid!")
+        self.price = self.price + 1
